6206dcd9dfb1d4fcee995dccfa3559f0c1216604
Pat Haynes <pathaynes32@gmail.com>
2019-10-08 14:07:15 -0700

    stuff

--- e2e/__tests__/me.test.js
+++ e2e/__tests__/me.test.js
@@ -12,9 +12,9 @@ describe('Ski Resorts API', () => {
   });
 
   const skiResort = {
-    name: 'RevelStoke',
+    name: 'Revel Stoke',
     location: 'British Columbia',
-    lifts: 2,
+    lifts: 3,
     elevation: '5000'
   };
 
@@ -97,9 +97,9 @@ describe('Ski Resorts API', () => {
                     "__v": 0,
                     "_id": Any<String>,
                     "elevation": 5000,
-                    "lifts": 2,
+                    "lifts": 3,
                     "location": "British Columbia",
-                    "name": "RevelStoke",
+                    "name": "Revel Stoke",
                     "owner": Any<String>,
                   }
                 `
